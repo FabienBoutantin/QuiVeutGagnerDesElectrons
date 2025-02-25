e451336331027988bcba3bf27c122fff1e0ad2f1
Fabien Boutantin <48051913+FabienBoutantin@users.noreply.github.com>
2025-02-25 21:06:41 +0100

    feat: reworked the question page for simplicity and perf

--- pages/question.py
+++ pages/question.py
@@ -27,74 +27,50 @@ from pages.base_page import Page
 from utils import get_logo_surf
 
 
-# Top, left, width, height
-QUESTION_RECT = pygame.Rect(
-    (QUESTION_SPAN, HEIGHT // 2 - QUESTION_HEIGHT // 2),
-    (WIDTH - QUESTION_SPAN * 2, QUESTION_HEIGHT)
-)
-
-
-def get_answer_rect_list() -> list:
-    """
-    Returns a list of pygame.Rect objects representing the answer rectangles.
-    """
-    result = []
-    for y in range(2):
-        for x in range(2):
-            left = x * (WIDTH // 2) + ANSWER_SPAN
-            top = HEIGHT // 2 + QUESTION_HEIGHT + y * ANSWER_HEIGHT
-            top += y * ANSWER_SPAN // 2
-            width = WIDTH // 2 - ANSWER_SPAN * 2
-            height = HEIGHT // 4 - ANSWER_HEIGHT
-            result.append(pygame.Rect(left, top, width, height))
-    return result
-
-
-ANSWER_RECTS = get_answer_rect_list()
-
 ANSWER_KEYS = (pygame.K_a, pygame.K_b, pygame.K_c, pygame.K_d)
 
 
-def draw_cartouche(screen, rect, span, selection=False):
+def draw_cartouche(surface, span, selection=False, line_width=4):
     """
     Draws a cartouche (decorative frame) around a given rectangle on
-    the given screen.
+    the given surface.
     """
     if selection:
         back_color = ANSWER_SELECTION_COLOR
     else:
         back_color = ANSWER_BACKGROUND_COLOR
+    w, h = surface.get_size()
+    main_rect = pygame.Rect((span, 0), (w - 2*span, h))
     # Main rect
-    pygame.draw.rect(screen, back_color, rect, 0, span)
-    pygame.draw.rect(screen, ANSWER_LINE_COLOR, rect, 4, span)
+    pygame.draw.rect(surface, back_color, main_rect, 0, span)
+    pygame.draw.rect(surface, ANSWER_LINE_COLOR, main_rect, line_width, span)
 
-    # Left lines
-    pygame.draw.rect(
-        screen,
-        ANSWER_LINE_COLOR,
-        (rect.left - span, rect.centery - 10, span, 20),
-        4
-    )
-    pygame.draw.rect(
-        screen,
-        back_color,
-        (rect.left - span, rect.centery - 10 + 4, span + 4, 20 - 2 * 4),
-        0
-    )
-
-    # Right lines
-    pygame.draw.rect(
-        screen,
-        ANSWER_LINE_COLOR,
-        (rect.right, rect.centery - 10, span, 20),
-        4
-    )
-    pygame.draw.rect(
-        screen,
-        back_color,
-        (rect.right - 4, rect.centery - 10 + 4, span + 4, 20 - 2 * 4),
-        0
-    )
+    thickness = h // 4
+    for left in (0, main_rect.right):
+        pygame.draw.rect(
+            surface,
+            ANSWER_LINE_COLOR,
+            (
+                left,
+                main_rect.centery - thickness // 2,
+                span,
+                thickness
+            ),
+            line_width
+        )
+        pygame.draw.rect(
+            surface,
+            back_color,
+            (
+                left - line_width,
+                main_rect.centery - thickness // 2 + line_width,
+                span + 2 * line_width,
+                thickness - 2 * line_width
+            )
+        )
+    main_rect.left += span
+    main_rect.width -= 2 * span
+    return main_rect
 
 
 # pylint: disable=too-many-instance-attributes
@@ -107,23 +83,89 @@ class QuestionPage(Page):
         Initializes the QuestionPage object with the given question list.
         """
         super().__init__()
+        self.highlighted = None
         self.question_list = question_list
-        self.current_question_idx = self.question_list.current_question_idx()
+        self.question = self.question_list.get_current_question()
+
+        self.init_logo()
+        self.init_question()
+        self.init_lifelines()
+
+    def init_logo(self):
+        """
+        Initializes the logo surface and position.
+        """
         self.logo_surface = get_logo_surf()
-        self.logo_pos_x = (WIDTH - self.logo_surface.get_size()[0]) // 2
-        self.logo_pos_y = HEIGHT // 2 - self.logo_surface.get_size()[1]
-        self.logo_pos_y //= 2
-        self.logo_pos_y -= 20
+        x = (WIDTH - self.logo_surface.get_width()) // 2
+        y = HEIGHT // 2 - self.logo_surface.get_height()
+        y //= 2
+        y -= 20
+        self.logo_pos = (x, y)
 
-        question = self.question_list.get_current_question()
-
-        self.question_txt_surf = fonts.render_text_at_best(
+    def init_question(self):
+        """
+        Initializes the question and answer surfaces.
+        """
+        self.question_surf = pygame.surface.Surface(
+            (WIDTH, QUESTION_HEIGHT), pygame.SRCALPHA
+        )
+        inner_rect = draw_cartouche(self.question_surf, QUESTION_SPAN)
+        question_txt_surf = fonts.render_text_at_best(
             fonts.normal(),
-            question.text,
+            self.question.text,
             QUESTION_TEXT_COLOR,
-            QUESTION_RECT.width - 2 * QUESTION_SPAN, QUESTION_RECT.height
+            inner_rect.width, inner_rect.height
+        )
+        w, h = question_txt_surf.get_size()
+        self.question_pos = (0, (HEIGHT - QUESTION_HEIGHT) // 2)
+        self.question_surf.blit(
+            question_txt_surf,
+            ((WIDTH - w) // 2, (self.question_surf.get_height() - h) // 2)
         )
 
+        font = fonts.normal()
+        self.answer_surfaces = []
+        for i in range(4):
+            surfaces = []
+            for selected in (False, True):
+                s = pygame.surface.Surface(
+                    (WIDTH // 2, ANSWER_HEIGHT), pygame.SRCALPHA
+                )
+                inner_rect = draw_cartouche(s, ANSWER_SPAN, selected)
+                answer_txt = font.render(
+                    f'{chr(ord("A")+i)} : ', True, QUESTION_TEXT_COLOR
+                )
+                x = inner_rect.left
+                y = (inner_rect.height - answer_txt.get_height()) // 2
+                s.blit(answer_txt, (x, y))
+                x += answer_txt.get_width()
+                if selected:
+                    txt_color = (0, 0, 255)
+                else:
+                    txt_color = DEFAULT_TEXT_COLOR
+                answer_txt = fonts.render_text_at_best(
+                    font,
+                    self.question.answers[i],
+                    txt_color,
+                    inner_rect.width - answer_txt.get_width(),
+                    inner_rect.height
+                )
+                y = (inner_rect.height - answer_txt.get_height()) // 2
+                s.blit(answer_txt, (x, y))
+                surfaces.append(s)
+
+            x = WIDTH // 2 * (i % 2)
+            y = self.question_pos[1] + self.question_surf.get_height()
+            y += ANSWER_SPAN
+            if i > 1:
+                y += ANSWER_HEIGHT + ANSWER_SPAN
+            rect = pygame.Rect((x, y), (WIDTH // 2, ANSWER_HEIGHT))
+            self.answer_surfaces.append((surfaces, rect))
+
+    def init_lifelines(self):
+        """
+        Initializes the lifeline buttons.
+        """
         self.buttons = []
         # 50:50 button
         action = self.question_list.use_fifty
@@ -169,32 +211,65 @@ class QuestionPage(Page):
         Handles the given event.
         """
         if event.type == pygame.KEYDOWN:
-            if event.key in ANSWER_KEYS:
-                answer = ANSWER_KEYS.index(event.key)
-                self.question_list.validate_answer(answer)
+            self.handle_key(event.key)
+        elif event.type == pygame.MOUSEMOTION:
+            self.handle_mouse_motion()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            for i in range(4):
-                if ANSWER_RECTS[i].collidepoint(mouse_pos):
-                    self.question_list.validate_answer(i)
-            for _, rect, action, used in self.buttons:
-                if not used() and rect.collidepoint(mouse_pos):
-                    action()
+            self.handle_mouse_button_down()
+
+    def handle_key(self, key):
+        """
+        Handles the given key press event.
+        """
+        try:
+            answer = ANSWER_KEYS.index(key)
+            self.question_list.validate_answer(answer)
+        except ValueError:
+            pass
+
+    def handle_mouse_motion(self):
+        """
+        Handles the mouse motion event.
+        """
+        mouse_pos = pygame.mouse.get_pos()
+        self.highlighted = None
+        for i, (_, rect) in enumerate(self.answer_surfaces):
+            if rect.collidepoint(mouse_pos):
+                self.highlighted = i
+                return
+
+    def handle_mouse_button_down(self):
+        """
+        Handles the mouse button down event.
+        """
+        mouse_pos = pygame.mouse.get_pos()
+        for i, (_, rect) in enumerate(self.answer_surfaces):
+            if rect.collidepoint(mouse_pos):
+                self.question_list.validate_answer(i)
+                return
+        for _, rect, action, used in self.buttons:
+            if not used() and rect.collidepoint(mouse_pos):
+                action()
+                return
 
     def draw(self, screen, cur_time, dt):
         """
         Draws the question and answers on the screen.
         """
-        question = self.question_list.get_current_question()
-
         screen.blit(
-            self.logo_surface, (self.logo_pos_x, self.logo_pos_y)
+            self.logo_surface, self.logo_pos
         )
 
-        self.draw_question(screen)
+        screen.blit(
+            self.question_surf,
+            self.question_pos
+        )
 
-        for i, answer in enumerate(question.answers):
-            self.draw_proposition(screen, i, answer, fonts.small())
+        for i, (surfs, rect) in enumerate(self.answer_surfaces):
+            screen.blit(
+                surfs[self.highlighted == i],
+                rect.topleft
+            )
 
         for txt, rect, _, used in self.buttons:
             if not used() and rect.collidepoint(pygame.mouse.get_pos()):
@@ -220,43 +295,3 @@ class QuestionPage(Page):
                     rect.bottomright,
                     15
                 )
-
-    def draw_question(self, screen):
-        """
-        Draws the question on the screen.
-        """
-        draw_cartouche(screen, QUESTION_RECT, QUESTION_SPAN)
-
-        w, h = self.question_txt_surf.get_size()
-        screen.blit(
-            self.question_txt_surf,
-            (
-                QUESTION_RECT.centerx - w // 2,
-                QUESTION_RECT.centery - h // 2
-            )
-        )
-
-    def draw_proposition(self, screen, i, text, font):
-        """
-        Draws the given proposition on the screen.
-        """
-        is_selected = ANSWER_RECTS[i].collidepoint(pygame.mouse.get_pos())
-        draw_cartouche(
-            screen,
-            ANSWER_RECTS[i], ANSWER_SPAN,
-            selection=is_selected
-        )
-
-        if is_selected:
-            txt_color = (0, 0, 255)
-        else:
-            txt_color = DEFAULT_TEXT_COLOR
-        answer_txt = font.render(
-            f'{chr(ord("A")+i)} : ', True, QUESTION_TEXT_COLOR
-        )
-        x = ANSWER_RECTS[i].left + ANSWER_HEIGHT // 2
-        y = ANSWER_RECTS[i].centery - answer_txt.get_height() // 2
-        screen.blit(answer_txt, (x, y))
-        x += answer_txt.get_width()
-        answer_txt = font.render(text, True, txt_color)
-        screen.blit(answer_txt, (x, y))
